decb2a8eb0bd387a956590c8e1e97ed978cc6683
Adrian Edelen <adrianedelen@gmail.com>
2020-10-09 14:36:05 -0400

    major changes
    moved all content to one page, changed bg color, fixed up html, other minor changes

--- Scripts/Custom.js
+++ Scripts/Custom.js
@@ -1,4 +1,4 @@
-﻿// event listener for the button
+﻿// event listener for the do it button
 document.getElementById("calculate").addEventListener("click", () => {
 	//declaring vars
 	let b1 = parseInt(document.getElementById("building1").value);
@@ -7,17 +7,10 @@ document.getElementById("calculate").addEventListener("click", () => {
 	let b4 = parseInt(document.getElementById("building4").value);
 	let b5 = parseInt(document.getElementById("building5").value);
 	let b6 = parseInt(document.getElementById("building6").value);
-
 	//putting the vars in an array to work with
 	let bldgArr = [b1, b2, b3, b4, b5, b6];
-	// clear the colors of from last time
-	for (i = 0; i < bldgArr.length; i++) {
-		if (document.getElementById("building" + (i + 1)).className.match(/(?:^|\s)bg-warning(?!\S)/)) {
-			document.getElementById("building" + (i + 1)).classList.remove("bg-warning");
-		} else {
-			continue;
-		}
-	}
+	//clear the colors of from last time
+	clean(bldgArr);
 	//iterate through the loop and find each building that can see the sun and store it in an array
 	let bigArr = new Array();
 	let bigTemp = -1;
@@ -27,7 +20,6 @@ document.getElementById("calculate").addEventListener("click", () => {
 			bigArr.push("prog" + (i + 1));
 			console.log(i);
 		}
-
 	}
 	//apply the yellow color to the buildings that can see the sun
 	for (let i = 0; i < bigArr.length; i++) {
@@ -38,7 +30,7 @@ document.getElementById("calculate").addEventListener("click", () => {
 	function progFill() {
 		for (let i = 0; i <= bldgArr[0] * 10; i++) {
 			let prog = 'prog1';
-			sleep1(i,prog);
+			sleep1(i, prog);
 		}
 		for (let j = 0; j <= bldgArr[1] * 10; j++) {
 			let prog = 'prog2';
@@ -54,19 +46,18 @@ document.getElementById("calculate").addEventListener("click", () => {
 		}
 		for (let z = 0; z <= bldgArr[4] * 10; z++) {
 			let prog = 'prog5';
-			sleep1(z, prog);	
+			sleep1(z, prog);
 		}
 		for (let x = 0; x <= bldgArr[5] * 10; x++) {
 			let prog = 'prog6';
-			sleep1(x,prog);
+			sleep1(x, prog);
 		}
 	}
 	//this function is called in the previous loops, this fills the bars 1% at a time
 	//with a 10ms delay between each 'tick'
-	function sleep1(i,prog) {
+	function sleep1(i, prog) {
 		setTimeout(function () {
 			document.getElementById(prog).style.height = i + "%";
-			
 		}, 10 * i)
 	}
 })
@@ -87,5 +78,35 @@ document.querySelectorAll(".numbersOnly").forEach(a => {
 		}
 	})
 })
+//clean resets the coloring of the buildings
+function clean(bldgArr) {
+	for (i = 0; i < bldgArr.length; i++) {
+		let bldg = document.getElementById("prog" + (i + 1))
+		bldg.classList.remove("bg-warning");
 
+	}
+
+}
+//clears text out of the text boxes on click
+function cleanText(el) {
+	document.getElementById(el).value = "";
+}
+document.getElementById("building1").addEventListener("click", () => {
+	cleanText("building1");
+})
+document.getElementById("building2").addEventListener("click", () => {
+	cleanText("building2");
+})
+document.getElementById("building3").addEventListener("click", () => {
+	cleanText("building3");
+})
+document.getElementById("building4").addEventListener("click", () => {
+	cleanText("building4");
+})
+document.getElementById("building5").addEventListener("click", () => {
+	cleanText("building5");
+})
+document.getElementById("building6").addEventListener("click", () => {
+	cleanText("building6");
+})
 
